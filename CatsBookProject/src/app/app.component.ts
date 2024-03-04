8b34fffd56ea52e2102a799d8d2da4f7ae324d28
ameralmunajjed <a.munajed@icloud.com>
2024-03-04 02:19:50 +0100

    for loop test

--- CatsBookProject/src/app/app.component.ts
+++ CatsBookProject/src/app/app.component.ts
@@ -13,19 +13,19 @@ import {StroyComponent} from './stroy/stroy.component';
 })
 export class AppComponent {
   title = 'CatsBookProject';
-  postText =[
-    'Hello , i am here to meet new friends and make a friends relationship',
-    'Hello everyone, Iam glad you are here',
-    'Hey, my name is Susanne. I amm 2 years old',
-       'I like eating cookies.'
-   ]
-  
+    postText =[
+      'Hello , i am here to meet new friends and make a friends relationship',
+      'Hello everyone, Iam glad you are here',
+      'Hey, my name is Susanne. I amm 2 years old',
+         'I like eating cookies.'
+     ]
+    
   postImgs =[
-    'assets/Imge/img1.png',
-    'assets/Imge/im21.png',
-    'assets/Imge/img3.jpg',
-    'assets/Imge/img4.jpg',
-   ]
+      'assets/Imge/img1.png',
+      'assets/Imge/im21.png',
+      'assets/Imge/img3.jpg',
+      'assets/Imge/img4.jpg',
+     ]
 
    buttonClicked(){
     alert("What's up");
